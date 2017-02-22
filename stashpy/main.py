2dda52426b46036bb7dac612509a92e71facaa5a
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2017-02-22 16:37:59 +0100

    Refactored and extended RabbitApp, tests

--- stashpy/main.py
+++ stashpy/main.py
@@ -5,6 +5,11 @@ import logging.config
 
 import tornado.ioloop
 import yaml
+try:
+    import kombu
+    import kombu.mixins
+except ImportError:
+    pass
 
 from .handler import MainHandler
 from stashpy import constants
@@ -28,21 +33,42 @@ class TornadoApp:
             io_loop.start()
 
 
+class MessageConsumer(kombu.mixins.ConsumerMixin):
+
+    def __init__(self, connection, queue_name, exchange):
+        self.connection = connection
+        self.task_queue = kombu.Queue(queue_name,
+                                      kombu.Exchange(exchange), '')
+
+    def get_consumers(self, Consumer, channel):
+        return [Consumer(queues=[self.task_queue],
+                         callbacks=[self.on_task])]
+
+    def on_task(self, body, message):
+        #TODO here is where the magic happes
+        logger.info('Processing new message on queue %s: body %s // payload %s',
+                    self.task_queue.name,
+                    body,
+                    message.payload)
+        message.ack()
+
+
 class RabbitApp:
 
     def __init__(self, config):
         self.config = config
+        connection = kombu.Connection(self.config['queue_config']['url'])
+        self.consumer = MessageConsumer(
+            connection,
+            self.config['queue_config']['queuename'],
+            self.config['queue_config']['exchange'])
 
     def run(self):
-        from kombu import Connection
-        with Connection(self.config['queue_config']['url']) as conn:
-            logger.info("Stashpy started, listening to messages on {}:{}".format(
-                self.config['queue_config']['url'],
-                self.config['queue_config']['queuename']))
-            simple_queue = conn.SimpleQueue('simple_queue')
-            message = simple_queue.get(block=True, timeout=1)
-            message.ack()
-            simple_queue.close()
+        logger.info("Stashpy started, listening to messages on {}:{}".format(
+            self.config['queue_config']['url'],
+            self.config['queue_config']['queuename']))
+        self.consumer.run()
+
 
 CONFIG_ERR_MSG = 'Either one of tcp_config or queue_config are allowed'
 
--- stashpy/tests/unit/test_queue_app.py
+++ stashpy/tests/unit/test_queue_app.py
@@ -1,15 +1,51 @@
 import unittest
 from unittest import mock
-from stashpy.main import RabbitApp
+from types import SimpleNamespace
+from stashpy.main import RabbitApp, MessageConsumer
+
+class ConsumerStub:
+    def __init__(self, queues, callbacks):
+        self.queues = queues
+        self.callbacks = callbacks
+
+class MessageStub(SimpleNamespace):
+
+    def ack(self):
+        self.acked = True
 
 class QueueAppTests(unittest.TestCase):
 
+    @mock.patch('stashpy.main.MessageConsumer')
     @mock.patch('kombu.Connection')
-    def test_run(self, mock_connection_class):
-        app = RabbitApp({'queue_config': {'url': 'amqp://thequeueurl', 'queuename': 'aq'}})
-        app.run()
-        self.assertEqual(mock_connection_class.call_count, 1)
-        mock_connection_class.assert_called_once_with('amqp://thequeueurl')
-        self.assertEqual(mock_connection_class.return_value.__enter__.call_count, 1)
-        mock_connection = mock_connection_class.return_value.__enter__.return_value
-        self.assertEqual(mock_connection.SimpleQueue.call_count, 1)
+    def test_run(self, mock_connection, mock_consumer):
+        config = {'queue_config': {'url': 'amqp://thequeueurl',
+                                   'queuename': 'aq',
+                                   'exchange': 'logging'}}
+        app = RabbitApp(config)
+        self.assertEqual(mock_consumer.call_count, 1)
+        mock_consumer.assert_called_once_with(
+            mock_connection.return_value,
+            'aq', 'logging')
+
+
+    @mock.patch('stashpy.main.kombu')
+    def test_consumer(self, mock_kombu):
+        consumer = MessageConsumer(mock.MagicMock(), 'aq', 'logging')
+        self.assertEqual(mock_kombu.Queue.call_count, 1)
+        self.assertEqual(mock_kombu.Exchange.call_count, 1)
+        mock_kombu.Exchange.assert_called_once_with('logging')
+        mock_kombu.Queue.assert_called_once_with(
+            'aq', mock_kombu.Exchange.return_value, '')
+        consumers = consumer.get_consumers(ConsumerStub, None)
+        self.assertEqual(len(consumers), 1)
+        self.assertTrue(consumers[0].queues[0] is consumer.task_queue)
+        # is check does not work here. confusion.
+        self.assertEqual(consumers[0].callbacks[0], consumer.on_task)
+
+
+    @mock.patch('stashpy.main.kombu')
+    def test_consumer_on_task(self, mock_kombu):
+        consumer = MessageConsumer(mock.MagicMock(), 'aq', 'logging')
+        message = MessageStub(payload='I am a string, yo')
+        consumer.on_task(None, message)
+        self.assertTrue(message.acked)
